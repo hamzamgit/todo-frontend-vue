be7e1784842fe6b941344c42b7ef9aa9602eff4c
Hamza Mushtaq <hamza1mushtaq@gmail.com>
2021-05-30 04:35:34 +0500

    init

---
+++ tests/unit/app.spec.ts
@@ -0,0 +1,12 @@
+import {mount, shallowMount} from '@vue/test-utils'
+import HelloWorld from '@/components/HelloWorld.vue'
+import Header from "@/components/Header.vue";
+
+describe('Header.vue', () => {
+  it('renders props.msg when passed', () => {
+    const msg = 'THE TODO APP'
+    const wrapper = mount(Header, {
+    })
+    expect(wrapper.text()).toMatch(msg)
+  })
+})
